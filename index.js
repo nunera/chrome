d80fb5d8acf8a12790aaa97362f96da321c9ef87
nunera <67439647+nunera@users.noreply.github.com>
2023-10-04 23:54:50 +0000

    encode

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ form.addEventListener('submit', async event => {
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        console.warn(__uv$config.encodeUrl(url));
     });
 });
 
