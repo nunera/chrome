2566d5f68f0da07a3ed9a381af3b1ef9faa9687f
nunera <67439647+nunera@users.noreply.github.com>
2023-10-04 23:52:23 +0000

    e

--- uv/uv.bundle.js
+++ uv/uv.bundle.js
@@ -35153,14 +35153,10 @@ __webpack_require__.r(__webpack_exports__);
 // -------------------------------------------------------------
 const xor = {
     encode(str){
-        if (!str) return str;
-        return encodeURIComponent(str.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
+        return str;
     },
     decode(str){
-        if (!str) return str;
-        let [ input, ...search ] = str.split('?');
-
-        return decodeURIComponent(input).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char).join('') + (search.length ? '?' + search.join('?') : '');
+        return str;
     },
 };
 
