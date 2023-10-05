91c503dec8b2a0e51ff07382142800fcb9daa4db
nunera <67439647+nunera@users.noreply.github.com>
2023-10-05 07:14:18 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: ['https://tomp.app/','https://bare-server.nunera.workers.dev/'],
+    bare: 'https://bare-server.nunera.workers.dev/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
